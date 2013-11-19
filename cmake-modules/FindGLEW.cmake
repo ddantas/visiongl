f7be2e55b1d172ceb35e10d37a0bcb18f5903041
Helton Leal <helton.danilo@gmail.com>
2013-11-19 20:53:59 -0300

    cmake find
    some find tests

--- cmake-modules/FindGLEW.cmake
+++ cmake-modules/FindGLEW.cmake
@@ -1,49 +1,20 @@
-#
-# Try to find GLEW library and include path.
+# - Try to find GLEW
 # Once done this will define
-#
-# GLEW_FOUND
-# GLEW_INCLUDE_PATH
-# GLEW_LIBRARY
-# 
+#  GLEW_FOUND - System has GLEW
+#  GLEW_INCLUDE_DIRS - The GLEW include directories
+#  GLEW_LIBRARIES - The libraries needed to use GLEW
+#  GLEW_DEFINITIONS - Compiler switches required for using GLEW
 
-IF (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-		$ENV{PROGRAMFILES}/GLEW/include
-		$ENV{USERPROFILE}/GLEW/include
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
-		DOC "The directory where GL/glew.h resides")
-	FIND_LIBRARY( GLEW_LIBRARY
-		NAMES glew GLEW glew32 glew32s
-		PATHS
-		$ENV{PROGRAMFILES}/GLEW/lib
-		$ENV{USERPROFILE}/GLEW/lib
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
-		DOC "The GLEW library")
-ELSE (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-		/usr/include
-		/usr/local/include
-		/sw/include
-		/opt/local/include
-		DOC "The directory where GL/glew.h resides")
-	FIND_LIBRARY( GLEW_LIBRARY
-		NAMES GLEW glew
-		PATHS
-		/usr/lib64
-		/usr/lib
-		/usr/local/lib64
-		/usr/local/lib
-		/sw/lib
-		/opt/local/lib
-		DOC "The GLEW library")
-ENDIF (WIN32)
-
-IF (GLEW_INCLUDE_PATH)
-	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ELSE (GLEW_INCLUDE_PATH)
-	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ENDIF (GLEW_INCLUDE_PATH)
-
-MARK_AS_ADVANCED( GLEW_FOUND )
+find_package(GLEW REQUIRED)
+if (!GLEW_FOUND)
+{
+	if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+	{
+		set(possible_paths 
+		  "C:/Program Files/Microsoft Visual Studio"
+		  "${CSIDL_PROGRAM_FILES}/Microsoft Visual Studio"
+		  ${CSIDL_PROGRAM_FILES}
+		)
+		message("possiveis caminhos: " ${possible_paths})	
+	}
+}
