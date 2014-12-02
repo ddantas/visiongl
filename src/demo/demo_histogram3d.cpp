77b68db4e2a7fea3211a011fd8899d62f3cfe939
Helton Leal(Seeker) <helton.danilo@gmail.com>
2014-12-02 05:31:16 -0300

    Revised demo_histogram3d, demo_histogram2d left to revise.

--- src/demo/demo_histogram3d.cpp
+++ src/demo/demo_histogram3d.cpp
@@ -22,9 +22,9 @@ int main(int argc, char* argv[])
 
 	printf("VisionCL on %s\n\n",image_path);
 
+    //REMEMBER, LOAD A GRAY LEVEL IMAGE
     VglImage* img = vglDcmtkLoadDicom(image_path);
-    VglImage* out = vglCreate3dImage(cvSize(img->shape[VGL_WIDTH],img->shape[VGL_HEIGHT]),img->depth,1,img->shape[VGL_LENGTH]);
-	//vglNdarray3To4Channels(img);
+    VglImage* out = vglCreate3dImage(cvSize(img->shape[VGL_WIDTH],img->shape[VGL_HEIGHT]),img->depth,img->nChannels,img->shape[VGL_LENGTH]);
 	
     //OpenCL histogram
     TimerStart();
@@ -33,17 +33,12 @@ int main(int argc, char* argv[])
     TimerStart();
     int* histogram = vglClHistogram(img);
     printf("Testing clHistogram time: %s\n", getTimeElapsedInSeconds());
-    
 
     //CPU histogram
     TimerStart();
     int* histogram_cpu_r = (int*) malloc(256*sizeof(int));
-    int* histogram_cpu_g = (int*) malloc(256*sizeof(int));
-    int* histogram_cpu_b = (int*) malloc(256*sizeof(int));
 
     memset(histogram_cpu_r,0x0,256*sizeof(int));
-    memset(histogram_cpu_g,0x0,256*sizeof(int));
-    memset(histogram_cpu_b,0x0,256*sizeof(int));
 
     unsigned char* pixels = (unsigned char*) img->ndarray;
 
@@ -51,17 +46,13 @@ int main(int argc, char* argv[])
     {
       histogram_cpu_r[pixels[i]]++;
     }
+
     printf("Testing Histogram CPU time: %s\n", getTimeElapsedInSeconds());
     //Test if they are equal
     int t = 0;
     for(int i = 0; i < 256; i++)
     {
-      /*if (i % 3 == 0)
-        t += histogram_cpu_r[i/3] - histogram[i];
-      if (i % 3 == 1)
-        t += histogram_cpu_g[i/3] - histogram[i];
-      if (i % 3 == 2)
-        t += histogram_cpu_b[i/3] - histogram[i];*/
+      t += histogram_cpu_r[i] - histogram[i];
     }
     printf("Total diff %d\n", t);
 
