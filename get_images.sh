1254d8a8d96ee14accc8c1d693c392b592c68f00
Daniel Dantas <dantasu@gmail.com>
2019-01-10 20:08:54 -0300

    Uncommented lines in get_images.sh

--- get_images.sh
+++ get_images.sh
@@ -1,7 +1,7 @@
 
-#git clone https://github.com/ddantas/visiongl_images.git images
-#cd images
-#unzip mitosis.zip
-#cd ..
+git clone https://github.com/ddantas/visiongl_images.git images
+cd images
+unzip mitosis.zip
+cd ..
 
 echo Sample images downloaded to folder \"./images/\"
